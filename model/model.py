ba41aa55c35c0a9aea40a0c805b3e72962112ba5
Minyeob Kim <kimmy9809@gmail.com>
2022-08-02 12:25:54 +0900

    [FIX] reformat for taking args

--- model/model.py
+++ model/model.py
@@ -1,26 +1,26 @@
 import torch.nn as nn
 import torch.nn.functional as F
 from base import BaseModel
-from transformers import ElectraForSequenceClassification
+from transformers import AutoModelForSequenceClassification
 from typing import Dict
 
 
-class ElectraModel(BaseModel):
+class Transformers(AutoModelForSequenceClassification):
     def __init__(self, model_path: str, id2label: Dict[int,any], label2id: Dict[any,int],
-                num_labels=4, problem_type='multi_label_classification', trainable=True):
-        self.electra = ElectraForSequenceClassification.from_pretrained(
+                num_labels=4, problem_type='multi_label_classification', trainable=True, **kwargs):
+        self.model = self.from_pretrained(
             model_path,
             problem_type=problem_type,
             ignore_mismatched_sizes=True,
             num_labels=num_labels,
             id2label=id2label,
-            label2id=label2id,)
+            label2id=label2id)
         if not trainable:
             self.set_trainable(trainable)
 
     def set_trainable(self, trainable: bool):
         requires_grad = True if trainable else False
-        for param in self.electra.base_model.parameters():
+        for param in self.model.base_model.parameters():
             param.requires_grad = requires_grad
 
 
